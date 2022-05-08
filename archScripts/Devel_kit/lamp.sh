70caafacb5dc98f25ebcf5869796089127dfee03
Arya Bakhtiari <93792020+Torbatti@users.noreply.github.com>
2022-05-08 18:28:03 +0430

    added mysql secure installtion command

--- archScripts/Devel_kit/lamp.sh
+++ archScripts/Devel_kit/lamp.sh
@@ -7,3 +7,4 @@ sudo pacman -S mysql
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl start mysqld 
 sudo systemctl enable mysqld 
+sudo mysql_secure_installation 
