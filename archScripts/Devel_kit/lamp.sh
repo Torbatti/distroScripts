dd026bceb83c4c759c3e8d39ec81c88898b4e291
Arya Bakhtiari <93792020+Torbatti@users.noreply.github.com>
2022-05-08 18:26:46 +0430

    added mysql install db command

--- archScripts/Devel_kit/lamp.sh
+++ archScripts/Devel_kit/lamp.sh
@@ -1,6 +1,9 @@
 #install lamp arch
 sudo pacman -S apache 
-sudo systemctl start httpd 
+sudo systemctl start httpd
+sudo systemctl enable httpd
 sudo pacman -S php php-apache
 sudo pacman -S mysql 
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl start mysqld 
+sudo systemctl enable mysqld 
